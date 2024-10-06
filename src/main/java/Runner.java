2682d2d636ec0d84f791027266b0f6a3ae6c13b5
BerkhanSaral <137205976+BerkhanSaral@users.noreply.github.com>
2024-10-06 17:27:44 +0300

    t1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,8 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Commit 2");
         System.out.println("Commit 3");
+
+        System.out.println("techpro 1");
     }
 
 }
